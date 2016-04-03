ebb7f32c6ffa48b05b584bc27d0ae5b0eb26dc4a
Crevil <bjoern.soerensen@gmail.com>
2016-04-03 13:03:30 +0200

    Initial Commit

---
+++ DiskStatus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DiskStatus
+//
+//  Created by Bjørn Sørensen on 03/04/16.
+//  Copyright © 2016 Bjørn Sørensen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
